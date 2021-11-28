226cb684dcc748cd3ab6ac7f5de354d7ab03a42a
Gal Choen <76289001+GalCohen09@users.noreply.github.com>
2021-11-28 11:17:58 +0200

    Add files via upload

---
+++ MFCproject/MFCprojectDlg.h
@@ -0,0 +1,34 @@
+﻿
+// MFCprojectDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCprojectDlg dialog
+class CMFCprojectDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCprojectDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCPROJECT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
